90995080d226d38ab3b82144a49517a04f2bb6c3
yurii bilous <yurii.b@elogic.com.ua>
2023-06-17 16:22:27 +0300

    ko subscribe

--- app/code/Elogic/KoTutorial/view/frontend/web/js/view/test-view.js
+++ app/code/Elogic/KoTutorial/view/frontend/web/js/view/test-view.js
@@ -12,12 +12,17 @@ define([
     return Component.extend({
         defaults: {
             heading: ko.observable("ko heading"),
-            content: ko.observable('Lorem ipsum blallalala')
+            content: ko.observable('Lorem ipsum blallalala'),
+            counter: ko.observable(0)
         },
 
         initialize() {
             this._super();
             this.heading("update heading").content("Custom text from js");
+            this.content.subscribe(function (newValue) {
+                console.log("a change:" + newValue);
+                this.counter(newValue.length)
+            }, this)
         },
 
         onChangeInput: function () {
