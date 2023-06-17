fed4909d0ecbd46014362906ff3fd518bcfb59a6
yurii bilous <yurii.b@elogic.com.ua>
2023-06-17 16:48:39 +0300

    delaying changes notifications

--- app/code/Elogic/KoTutorial/view/frontend/web/js/view/test-view.js
+++ app/code/Elogic/KoTutorial/view/frontend/web/js/view/test-view.js
@@ -29,17 +29,21 @@ define([
             let self = this;
 
             setTimeout(function () {
-                self.heading("update heading"); //міняєм на те саме шо у нас вже є щоб впевнетись що сабскрайбер не спрацює, бо валує однакове
+                // self.heading("update heading"); //міняєм на те саме шо у нас вже є щоб впевнетись що сабскрайбер не спрацює, бо валує однакове
+                self.heading("update heading").content('New changes in content!');
             }, 4000);
 
             this.heading.subscribe(function (newValue) {
-                console.log("Headng value updated: " + newValue);
-                alert("Headng value updated: " + newValue);
+                // console.log("Headng value updated: " + newValue);
+                // alert("Headng value updated: " + newValue);
+                //
+                console.log("Headng value updated: " + newValue + " and content changed to: " + this.content());
             }, this);
 
             //завзяки цьому нотіфай - алвейс наш сабскрайбер буде відпрацьовувати завжди, не залежно яке значення
             this.heading.extend({
-                notify: 'always'
+                notify: 'always',
+                rateLimit: 50
             });
         },
 
