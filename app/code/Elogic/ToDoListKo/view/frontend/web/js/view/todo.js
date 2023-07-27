29e1d78eec28ffd6597aa8bf422125b5eb17898d
yurii bilous <yurii.b@elogic.com.ua>
2023-07-27 15:32:12 +0300

    styles

--- app/code/Elogic/ToDoListKo/view/frontend/web/js/view/todo.js
+++ app/code/Elogic/ToDoListKo/view/frontend/web/js/view/todo.js
@@ -18,6 +18,15 @@ define([
             this._super();
 
             this.isVisible(this.todos.length);
+        },
+
+        getProgressCount: function (completedTasks, totalTasks) {
+            if (isNaN(completedTasks) || isNaN(totalTasks)) {
+                return 0;
+            }
+
+            let percentage = parseInt(Math.floor(Math.round(completedTasks/totalTasks * 100)));
+            return !isNaN(percentage) ? percentage : 0;
         }
     });
 });
